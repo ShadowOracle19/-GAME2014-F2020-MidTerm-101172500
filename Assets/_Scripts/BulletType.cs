7ce39787e23615e994a562e1b64716ffbc1f0b03
ShadowOracle19 <lucas.g@hotmail.ca>
2020-10-20 11:37:56 -0400

    more headers

--- Assets/_Scripts/BulletType.cs
+++ Assets/_Scripts/BulletType.cs
@@ -1,4 +1,14 @@
-﻿using System.Collections;
+﻿////////////////////////////////////////////////////////////////////////////////////////////////////////
+//FileName: BulletType.cs
+//FileType: Visual C# Script
+//Author: Lucas Coates
+//Student Number: 101172500
+//Last Modified On: 10/20/2020 11:37 AM
+//Copy Rights: Mana Burn
+//Description: Handles the bullet types
+///////////////////////////////////////////////////////////////////////////////////////////////
+
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
--- Assets/_Scripts/IApplyDamage.cs
+++ Assets/_Scripts/IApplyDamage.cs
@@ -1,4 +1,13 @@
-﻿using System.Collections;
+﻿////////////////////////////////////////////////////////////////////////////////////////////////////////
+//FileName: IApplyDamage.cs
+//FileType: Visual C# Script
+//Author: Lucas Coates
+//Student Number: 101172500
+//Last Modified On: 10/20/2020 11:36 AM
+//Copy Rights: Mana Burn
+//Description: applies damage when bullets collide  
+//////////////////////////////////////////////////////////////////////////////////////////////////
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
